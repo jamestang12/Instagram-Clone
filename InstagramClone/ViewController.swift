de11e3ecc87436e0a0528a724a08b4d6adf5dcc5
jamestang12 <homing.tang@ryerson.ca>
2021-02-08 18:53:19 -0500

    first commit

---
+++ InstagramClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstagramClone
+//
+//  Created by James Tang on 2021-02-08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
