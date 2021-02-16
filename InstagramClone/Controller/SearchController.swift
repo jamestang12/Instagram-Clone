c09e24fc7179431bb22de21dde66267908568e06
jamestang12 <homing.tang@ryerson.ca>
2021-02-16 17:20:27 -0500

    Search Controller UI Update

--- InstagramClone/Controller/SearchController.swift
+++ InstagramClone/Controller/SearchController.swift
@@ -7,10 +7,38 @@
 
 import UIKit
 
-class SearchController: UIViewController {
+private let reuseIdentifier = "UserCell"
+
+class SearchController: UITableViewController {
+    
+    // MARK: - Properties
+    
+    // MARKL: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .systemBlue
+        configureTableView()
+    }
+    
+    // MARK: - Helpers
+    
+    func configureTableView(){
+        view.backgroundColor = .white
+        tableView.register(UserCell.self, forCellReuseIdentifier: reuseIdentifier)
+        tableView.rowHeight = 64
+    }
+}
+
+// MARK: - UITableViewDataSource
+
+extension SearchController{
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath)
+        return cell
     }
 }
---
+++ InstagramClone/View/UserCell.swift
@@ -0,0 +1,61 @@
+//
+//  UserCell.swift
+//  InstagramClone
+//
+//  Created by James Tang on 2021-02-16.
+//
+
+import UIKit
+
+class UserCell: UITableViewCell{
+    
+    // MARK: - Properties
+    private let profileImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFill
+        iv.clipsToBounds = true
+        iv.backgroundColor = .lightGray
+        iv.image = #imageLiteral(resourceName: "venom-7")
+        return iv
+    }()
+    
+    private let usernameLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.boldSystemFont(ofSize: 14)
+        label.text = "venom"
+        return label
+    }()
+    
+    private let fullnameLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.text = "Test User"
+        label.textColor = .lightGray
+        return label
+    }()
+    
+    // MARK: - Lifecycle
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        addSubview(profileImageView)
+        profileImageView.setDimensions(height: 48  , width: 48)
+        profileImageView.layer.cornerRadius = 48 / 2
+        
+        profileImageView.centerY(inView: self, leftAnchor: leftAnchor, paddingLeft: 12)
+        
+        let stack = UIStackView(arrangedSubviews: [usernameLabel, fullnameLabel])
+        stack.axis = .vertical
+        stack.spacing = 4
+        stack.alignment = .leading
+        
+        addSubview(stack)
+        stack.centerY(inView: profileImageView, leftAnchor: profileImageView.rightAnchor, paddingLeft: 8)
+    
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(docer:) has not been implemented")
+    }
+}
