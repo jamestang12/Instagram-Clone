5d0120f98c3e5a785b0000693c89252b5aa62050
jamestang12 <homing.tang@ryerson.ca>
2021-02-18 23:15:26 -0500

    Loading Indicator Created

--- InstagramClone/Controller/UploadPostController.swift
+++ InstagramClone/Controller/UploadPostController.swift
@@ -59,12 +59,15 @@ class UploadPostController: UIViewController{
     @objc func didTapDone(){
         guard let image = selectedImage else { return }
         guard let caption = captionTextView.text else { return }
+        
+        showLoader(true)
+        
         PostService.uploadPost(caption: caption, image: image) { (error) in
+            self.showLoader(false)
             if let error = error {
                 print("DEBUG: Failed to upload post \(error.localizedDescription)")
                 return
             }
-            
             self.delegate?.controllerDidFinishUploadingPost(self)
         }
     }
--- InstagramClone/Utils/Extensions.swift
+++ InstagramClone/Utils/Extensions.swift
@@ -10,12 +10,30 @@ import JGProgressHUD
 
 
 extension UIViewController{
+    static let hud = JGProgressHUD(style: .dark)
+
     func configureGradientLayer(){
         let gradient  = CAGradientLayer()
         gradient.colors = [UIColor.systemPurple.cgColor, UIColor.systemBlue.cgColor]
         gradient.locations = [0,1]
         view.layer.addSublayer(gradient)
         gradient.frame = view.frame
+    }
+    
+    func showLoader(_ show: Bool) {
+        view.endEditing(true)
+        
+        if show {
+            UIViewController.hud.show(in: view)
+        } else {
+            UIViewController.hud.dismiss()
+        }
+    }
+    
+    func showMessage(withTitle title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+        present(alert, animated: true, completion: nil)
     }
 }
 
@@ -32,7 +50,7 @@ extension UIButton{
 
 
 extension UIViewController {
-    static let hud = JGProgressHUD(style: .dark)
+//    static let hud = JGProgressHUD(style: .dark)
 //    
 //    func configureGradientLayer() {
 //        let gradient = CAGradientLayer()
@@ -42,21 +60,21 @@ extension UIViewController {
 //        gradient.frame = view.frame
 //    }
 //    
-    func showLoader(_ show: Bool) {
-        view.endEditing(true)
-        
-        if show {
-            UIViewController.hud.show(in: view)
-        } else {
-            UIViewController.hud.dismiss()
-        }
-    }
-    
-    func showMessage(withTitle title: String, message: String) {
-        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
-        present(alert, animated: true, completion: nil)
-    }
+//    func showLoader(_ show: Bool) {
+//        view.endEditing(true)
+//
+//        if show {
+//            UIViewController.hud.show(in: view)
+//        } else {
+//            UIViewController.hud.dismiss()
+//        }
+//    }
+//
+//    func showMessage(withTitle title: String, message: String) {
+//        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+//        alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+//        present(alert, animated: true, completion: nil)
+  //  }
 }
 
 extension UIButton {
