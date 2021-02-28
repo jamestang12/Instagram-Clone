409a08d597eba51377e41721e59a6433e16a122b
jamestang12 <homing.tang@ryerson.ca>
2021-02-28 18:16:17 -0500

    Notification UI Created

--- InstagramClone/Controller/NotificationController.swift
+++ InstagramClone/Controller/NotificationController.swift
@@ -7,10 +7,39 @@
 
 import UIKit
 
-class NotificationController: UIViewController {
+private let reuseIdentifier = "NotificationCell"
+
+class NotificationController: UITableViewController {
+    
+    // MARK: - Properties
+    
+    // MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+            configureTableView()
+
+    }
+    
+    // MARK: - Helpers
+    func configureTableView(){
+        view.backgroundColor = .white
+        navigationItem.title = "Notifications"
         
-        view.backgroundColor = .systemOrange
+        tableView.register(NotificationCell.self, forCellReuseIdentifier: reuseIdentifier)
+        tableView.rowHeight = 80
+        tableView.separatorStyle = .none
+    }
+
+}
+
+extension NotificationController{
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath) as! NotificationCell        
+        return cell
     }
 }
---
+++ InstagramClone/View/NotificationCell.swift
@@ -0,0 +1,91 @@
+//
+//  NotificationCell.swift
+//  InstagramClone
+//
+//  Created by James Tang on 2021-02-28.
+//
+
+import UIKit
+
+class NotificationCell: UITableViewCell{
+    
+    // MARK: - Properties
+    
+    private let profileImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFill
+        iv.clipsToBounds = true
+        iv.backgroundColor = .lightGray
+        return iv
+    }()
+    
+    private let infoLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.boldSystemFont(ofSize: 14)
+        label.text = "label......"
+        return label
+    }()
+    
+    private let postImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFill
+        iv.clipsToBounds = true
+        let tap = UITapGestureRecognizer(target: self, action: #selector(handlePostTapped))
+        iv.isUserInteractionEnabled = true
+        iv.addGestureRecognizer(tap)
+        iv.backgroundColor = .lightGray
+        return iv
+    }()
+    
+    private let followButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("title", for: .normal)
+        button.layer.cornerRadius = 3
+        button.layer.borderColor = UIColor.lightGray.cgColor
+        button.layer.borderWidth = 0.5
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        button.setTitleColor(.black, for: .normal)
+        button.addTarget(self, action: #selector(handleFollowTap), for: .touchUpInside)
+        return button
+    }()
+    
+    // MARK: - Lifecucle
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        selectionStyle = .none
+        
+        addSubview(profileImageView)
+        profileImageView.setDimensions(height: 48, width: 48)
+        profileImageView.layer.cornerRadius = 48/2
+        profileImageView.centerY(inView: self, leftAnchor: leftAnchor, paddingLeft: 12)
+        
+        addSubview(infoLabel)
+        infoLabel.centerY(inView: profileImageView, leftAnchor: profileImageView.rightAnchor, paddingLeft: 8)
+        
+        addSubview(followButton)
+        followButton.centerY(inView: self)
+        followButton.anchor(right: rightAnchor, paddingRight: 12, width: 100, height: 32)
+        
+        addSubview(postImageView)
+        postImageView.centerY(inView: self)
+        postImageView.anchor(right: rightAnchor, paddingRight: 12, width: 40, height: 40)
+        
+        followButton.isHidden = true
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    // MARK: - Actions
+    @objc func handleFollowTap(){
+        
+    }
+    
+    @objc func handlePostTapped(){
+        
+    }
+}
