20f83f820a676fb22636b2b15768543f13f34f51
jamestang12 <homing.tang@ryerson.ca>
2021-03-02 15:28:01 -0500

    Notification Data Model Created

---
+++ InstagramClone/Model/Notification.swift
@@ -0,0 +1,41 @@
+//
+//  Notification.swift
+//  InstagramClone
+//
+//  Created by James Tang on 2021-03-02.
+//
+
+import Firebase
+
+enum NotificationType: Int {
+    case like
+    case follow
+    case comment
+
+    var notificationMessage: String{
+        switch self {
+        case .like: return " liked your post. "
+        case .follow: return " started following you."
+        case .comment: return " commented on your post"
+        }
+    }
+}
+
+struct Notification {
+    let uid: String
+    var postImaheUrl: String?
+    var postId: String?
+    let timestamp: Timestamp
+    let type: NotificationType
+    let id: String
+    
+    init(dictionary: [String: Any]) {
+        self.timestamp = dictionary["timestamp"] as? Timestamp ?? Timestamp(date: Date())
+        self.id = dictionary["id"] as? String ?? ""
+        self.uid = dictionary["uid"] as? String ?? ""
+        self.postId = dictionary["postId"] as? String ?? ""
+        self.postImaheUrl = dictionary["postImaheUrl"] as? String ?? ""
+        self.type = NotificationType(rawValue: dictionary["type"] as? Int ?? 0) ?? .like
+
+    }
+}
